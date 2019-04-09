09482f59244db871e2faa5ccf6f98cea294c8829
diego <diego.aliaga@helsinki.fi>
2019-04-09 16:21:53 +0300

    from_mac

---
+++ flexpart_management/papers/Readme.md
@@ -0,0 +1,2 @@
+- Short summary of literature should go in the file : literature_summary.md
+- literature documents go to ./docs
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flexpart_management',
+    version='0.01',
+    packages=['flexpart_management'],
+    url='',
+    license='',
+    author='diego aliaga',
+    author_email='diego.aliaga@helsinki.fi',
+    description='package to run flexpart at taito', install_requires=['xarray', 'pandas']
+)
