1975107832d86b81b4110905e7390ba76dfd193f
diego <diego.aliaga@helsinki.fi>
2019-06-09 13:38:19 -0400

    from_mac

--- flexpart_management/notebooks/srun_launcher.sh
+++ flexpart_management/notebooks/srun_launcher.sh
@@ -4,5 +4,5 @@ for day in /homeappl/home/aliagadi/wrk/DONOTREMOVE/flexpart_management_data/runs
         echo ${day}
         srun -t20 -p serial --mem 1000 -c 1\
         python3 -u\
-        /homeappl/home/aliagadi/saltena_2018/flexpart_management/flexpart_management/notebooks/get_flx_log_pol_coords_taito_srun.py ${day}
+        /homeappl/home/aliagadi/saltena_2018/flexpart_management/flexpart_management/notebooks/get_flx_log_pol_coords_taito_srun.py ${day} &
     done
