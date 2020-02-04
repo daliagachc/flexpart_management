a8bca4472b23c51690a0b6d7277687296b604065
diego <diego.aliaga@helsinki.fi>
2020-02-04 13:11:23 +0200

    from_mac

--- flexpart_management/notebooks/george_data_analysisV02/n_02_ft_candidate_log.py
+++ flexpart_management/notebooks/george_data_analysisV02/n_02_ft_candidate_log.py
@@ -32,8 +32,8 @@ df = pd.read_excel(
     '/Volumes/mbProD/flexpart_management_data/flexpart_management/tmp_data/data_george_ccV02.xlsx')
 # %%
 # CONSTANTS
-t0,      t1,      name,   h11,h12,    h21,h22, tlen = \
-    't0_utc','t1_utc','name','h11','h12','h21','h22','length[h]'
+t0, t1, name, h11, h12, h21, h22, tlen = \
+    't0_utc', 't1_utc', 'name', 'h11', 'h12', 'h21', 'h22', 'length[h]'
 # %%
 ds = fa.open_temp_ds('ds_clustered_18.nc')
 da = ds[co.CONC]
@@ -41,16 +41,29 @@ da = ds[co.CONC]
 r = df.iloc[0]
 # %%
 log.ger.setLevel(log.log.INFO)
-for l,r in df.iterrows():
-    log.ger.info(f'doing {l}')
-    lfc.plot_plot(da, h11, h12, h21, h22, name, r, t0, t1, tlen)
 
+dzr = da.sum([co.RL, co.TH_CENTER]).load()
+# %%
+dv = (da / da['r_length'])
+# dvzr = dv.sum([co.RL,co.TH_CENTER]).load()
+
+dvzr = dv.sum([co.RL, co.TH_CENTER]).load()
+# %%
+dvzr = dvzr.assign_coords(km=dvzr[co.R_CENTER] * 100)
+import matplotlib.colors as colors
+
+vmax = dvzr.quantile(.9)
+vmin = vmax * 1e-3
+dvzr.plot(
+    # norm=colors.LogNorm(vmin=vmin, vmax=vmax),
+    # xscale='log',
+    vmax = vmax,
+    figsize = (8,4),
+    x='km', y=co.ZM)
+
+plt.show()
 
 # %%
-r = df[df['name']==11].iloc[0]
-
-# %%
-lfc.plot_plot(da, h11, h12, h21, h22, name, r, t0, t1, tlen)
 
 # %%
 # %%
